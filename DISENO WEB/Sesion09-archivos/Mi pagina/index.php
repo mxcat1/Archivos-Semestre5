0d5d425c602e0209ccdb90bc199018b7bb3252e7
mxcat1 <mxcat.xxxtremxx@gmail.com>
2018-05-30 00:51:34 -0500

    Signed-off-by: mxcat1 <mxcat.xxxtremxx@gmail.com>

---
+++ DISENO WEB/Sesion09-archivos/Mi pagina/index.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mi pagina web</title>
+    <link rel="stylesheet" href="css/estructura.css">
+</head>
+<body>
+<div class="contenedor">
+    <header>
+        <h1>Mi Pagina Web</h1>
+    </header>
+    <nav>
+
+        <div class="menu-cel">
+            <div class="menu1"></div>
+            <div class="menu1"></div>
+            <div class="menu1"></div>
+        </div>
+        <div class="menu-desplegable">
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Nosotros</a></li>
+                <li><a href="">Contacto</a></li>
+                <li><a href="">Ubicanos</a></li>
+            </ul>
+        </div>
+        <h3>Menu</h3>
+
+    </nav>
+    <section id="main">
+        <img src="img/colibri.jpg" alt="">
+        <p>
+            <?php include ("otros/msj.txt");
+            ?>
+        </p>
+    </section>
+    <aside>
+        <p>
+            <?php include ("otros/msj.txt");
+            ?>
+        </p>
+    </aside>
+    <footer>
+        <p>
+            <?php include ("otros/msj.txt");
+            ?>
+        </p>
+    </footer>
+</div>
+
+</body>
+</html>
