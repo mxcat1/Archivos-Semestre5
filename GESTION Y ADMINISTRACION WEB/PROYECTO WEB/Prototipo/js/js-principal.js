8ce883cfc8407bbd1da5f57782ace5e2b7bfcc9e
mxcat1 <mxcat.xxtremxx@gmail.com>
2018-06-04 12:57:15 -0500

    Signed-off-by: mxcat1 <mxcat.xxtremxx@gmail.com>

--- GESTION Y ADMINISTRACION WEB/PROYECTO WEB/Prototipo/js/js-principal.js
+++ GESTION Y ADMINISTRACION WEB/PROYECTO WEB/Prototipo/js/js-principal.js
@@ -1,4 +1,6 @@
 function cambiarimg() {
     const imagen=document.getElementById("imagen-galeria")
+    imagen.style.animationName = 'trancicion'
+    imagen.style.animationDuration= '3s'
     imagen.src="img/arequipa.jpg"
 }
