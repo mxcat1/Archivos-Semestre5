377b68d3951f76a60207d3b19006c3bcb5d7313b
mxcat1 <mxcat.xxxtremxx@gmail.com>
2018-06-05 00:41:56 -0500

    Signed-off-by: mxcat1 <mxcat.xxxtremxx@gmail.com>

---
+++ DISENO WEB/Ejemplo de pagina con flex/index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tarea1</title>
+    <link rel="stylesheet" href="css/estructura.css">
+</head>
+<body>
+<div id="contenedor">
+    <header>
+        <div id="cabecera">
+            <div id="titulo">
+                <h1>Titulo</h1>
+            </div>
+            <div id="logo">
+                <p>Logo</p>
+            </div>
+            <div id="menu">
+                <a href="">Inicio</a>
+                <a href="">Galeria</a>
+                <a href="">Nosotros</a>
+                <a href="">Contacto</a>
+            </div>
+        </div>
+        <div id="banner">
+            <div class="img-fake">Imagen</div>
+        </div>
+    </header>
+    <section>
+        <div id="noticias">
+            <h2>Noticias</h2>
+            <div class="img-fake" style="height: 200px">Imagen</div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Ad aperiam aspernatur, debitis delectus doloremque inventore
+                libero molestias officiis possimus! Nulla.</p>
+            <div class="img-fake" style="height: 80px">Imagen</div>
+            <div class="img-fake" style="height: 80px">Imagen</div>
+            <div class="img-fake" style="height: 80px">Imagen</div>
+        </div>
+        <div id="articulos">
+            <div id="articulo1">
+                <h2>Subtitulo</h2>
+                <div class="img-fake" style="height: 200px">Imagen</div>
+                <p>Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit.
+                    Corporis labore magni odit omnis
+                    saepe, sunt temporibus unde ut
+                    voluptas voluptatem!</p>
+            </div>
+            <div id="articulo2">
+                <h2>Subtitulo</h2>
+                <div class="img-fake" style="height: 200px">Imagen</div>
+                <p>Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit.
+                    Corporis labore magni odit omnis
+                    saepe, sunt temporibus unde ut
+                    voluptas voluptatem!</p>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <h2>Footer</h2>
+    </footer>
+
+</div>
+
+</body>
+</html>
