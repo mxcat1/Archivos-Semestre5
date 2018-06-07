fa8bbeaa2ee0edf16c941089b0f0228ad03fe6ce
mxcat1 <mxcat.xxxtremxx@gmail.com>
2018-06-07 00:00:34 -0500

    Signed-off-by: mxcat1 <mxcat.xxxtremxx@gmail.com>

---
+++ DISENO WEB/sesion10_archivos/Mi Pagina Casa/index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estructura.css">
+</head>
+<body>
+<div id="contenedor">
+    <header>
+        <div id="cabecera">
+            <div id="titulo">
+                <h1>TITULO</h1>
+            </div>
+            <div id="logo">
+                <img src="" alt="">
+            </div>
+            <div id="menu">
+                <ul>
+                    <a href="">INICIO</a>
+                    <a href="">GALERIA</a>
+                    <a href="">NOSOTROS</a>
+                    <a href="">CONTACTO</a>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <div id="sec-banner">
+        <div id="banner">
+            <img src="" alt="">
+        </div>
+        <div id="btn-next">
+
+        </div>
+        <div id="btn-previous">
+
+        </div>
+    </div>
+    <div id="datos">
+        <div id="p-texto">
+            <h2>SUBTITULO</h2>
+            <p>
+                Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit.
+                Aspernatur consectetur, delectus
+                iusto labore quidem quod repudiandae
+                sint tempora tempore! Autem!
+            </p>
+        </div>
+        <div id="imagenes">
+            <img src="" alt="">
+        </div>
+    </div>
+    <footer>
+        <p>Derechos Reservados</p>
+    </footer>
+</div>
+
+</body>
+</html>
