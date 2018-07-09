79d18191803e11ec62bb201ce9e636939893bb69
mxcat1 <mxcat.xxxtremxx@gmail.com>
2018-07-08 21:35:20 -0500

    Signed-off-by: mxcat1 <mxcat.xxxtremxx@gmail.com>

---
+++ GESTION Y ADMINISTRACION WEB/PROYECTO WEB/Prototipo/js/js-principal.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $("#ren-menu1").click(function () {
+        $("#resena-emp").toggle("3s").css("display","flex")
+    });
+    $("#ren-menu2").click(function () {
+        $("#identidad-emp").toggle("3s").css("display","flex")
+    });
+    $("#ren-menu3").click(function () {
+        $("#mision-vision-emp").toggle("3s").css("display","flex")
+    });
+    $("#ren-menu4").click(function () {
+        $("#valores-emp").toggle("3s").css("display","flex")
+    });
+    $("#ren-menu5").click(function () {
+        $("#organigrama-emp").toggle("3s").css("display","flex")
+    });
+});
